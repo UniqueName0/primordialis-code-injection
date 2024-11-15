84f9f6279923a29ed388c1f97385f7964fcc4680
Nathan <nathan.titirangi@gmail.com>
2024-11-16 11:00:47 +1300

    Revert "adjust build"
    This reverts commit e30193700dbc7e9c46543d931c48427d42f10c99.

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 # you probably want to use https://github.com/NathanSnail/Detours/tree/linux
 # which is just a mingw gcc makefile for detours so that you can compile everything on linux
-rm out/inject.dll
-rm out/PrimordialisModloader.exe
 x86_64-w64-mingw32-gcc --shared inject.c -o ./out/inject.dll -I Detours/src/ -L Detours/lib.X64 -l detours
 x86_64-w64-mingw32-gcc -o ./out/PrimordialisModloader.exe main.c
--- main.c
+++ main.c
@@ -25,7 +25,6 @@ LPWSTR ConvertToLPWSTR(const char* charArray) {
 
 
 int main() {
-  printf("Primordialis Modloader started\n");
   HANDLE ph; // process handle
   HANDLE rt; // remote thread
   LPVOID rb; // remote buffer
@@ -68,10 +67,5 @@ int main() {
 
   printf("created thread\n");
   CloseHandle(ph);
-  while (1) {
-    printf("Primordialis Modloader going to sleep");
-    Sleep(-1); // infinite sleep
-    printf("Primordialis Modloader awoke for some reason");
-  } // this makes the console not close
   return 0;
 }
