b26910296cf95dadc250af2aaaece397a3cb5527
Nathan <nathan.titirangi@gmail.com>
2024-11-16 00:57:14 +1300

    sane names

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 cl ./inject.c /LD /Fe:"./out/inject.dll" /I "./Detours/include" /link "./Detours/lib.X64/detours.lib"
-cl ./main.c /Fe:"./out/main.exe"
+cl ./main.c /Fe:"./out/PrimordialisModloader.exe"
--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 # you probably want to use https://github.com/NathanSnail/Detours/tree/linux
 # which is just a mingw gcc makefile for detours so that you can compile everything on linux
-x86_64-w64-mingw32-gcc --shared inject.c -o inject.dll -I Detours/src/ -L Detours/lib.X64 -l detours
+x86_64-w64-mingw32-gcc --shared inject.c -o ./out/inject.dll -I Detours/src/ -L Detours/lib.X64 -l detours
+x86_64-w64-mingw32-gcc -o ./out/PrimordialisModloader.exe main.c
