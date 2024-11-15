833de40a50c0cb7ff70b4f4a43d40e0d341c2d58
Nathan <nathan.titirangi@gmail.com>
2024-11-16 00:43:49 +1300

    add note about my linux build process

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# you probably want to use https://github.com/NathanSnail/Detours/tree/linux
+# which is just a mingw gcc makefile for detours so that you can compile everything on linux
 x86_64-w64-mingw32-gcc --shared inject.c -o inject.dll -I Detours/src/ -L Detours/lib.X64 -l detours
