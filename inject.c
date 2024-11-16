26b4fae7cda73d7557c73d8a2a0b05a95e6ed6e0
Nathan <nathan.titirangi@gmail.com>
2024-11-16 21:33:27 +1300

    do some development

--- inject.c
+++ inject.c
@@ -1,6 +1,7 @@
 #include "WindowsHLinux/windows_loader.h"
 DoesNothing;
 #include "debugTools.c"
+#include "modApi.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +13,7 @@ DoesNothing;
 	((value < 0x140000000) ? (baseAddress + value)                           \
 				     : (baseAddress + value - 0x140000000))
 
-uintptr_t baseAddress;
+size_t baseAddress;
 
 #define panic()                                                                \
 	printf("panic at %s:%d\n", __FILE__, __LINE__);                          \
@@ -26,19 +27,6 @@ uintptr_t baseAddress;
 		panic();                                                           \
 	}
 
-typedef struct {
-	char *name;
-	char *version;
-	char *entrydll;
-
-	void (*init)();
-} ModInfo;
-
-typedef struct {
-	int length;
-	ModInfo **mods;
-} ModList;
-
 void appendModToModList(ModList *modlist, ModInfo *mod) {
 	modlist->length++;
 	modlist->mods =
@@ -46,18 +34,21 @@ void appendModToModList(ModList *modlist, ModInfo *mod) {
 	modlist->mods[modlist->length - 1] = mod;
 }
 
-ModList *modlist;
+ModList *mod_list;
 #pragma comment(lib, "user32.lib")
 
-void ParseConfigFile(char *content, char *modname, ModInfo *info) {
-	char **fields[3] = {&info->name, &info->version, &info->entrydll};
-	printf("Parsing config for modid '%s'\n", modname);
+void ParseConfigFile(char *content, char *modID, ModInfo *mod) {
+	size_t id_len = strlen(modID);
+	mod->id = malloc(id_len);
+	memcpy(mod->id, modID, id_len);
+	char **fields[3] = {&mod->name, &mod->version, &mod->entrydll};
+	printf("Parsing config for modid '%s'\n", modID);
 	printf("%s\n", content);
 	for (int i = 0; i < 3; ++i) {
 		if (!*content) {
 			printf(
 			    "Config file for modid '%s' malformed, missing values\n",
-			    modname);
+			    modID);
 			panic();
 		}
 		char *last = content;
@@ -75,11 +66,11 @@ void ParseConfigFile(char *content, char *modname, ModInfo *info) {
 	}
 }
 
-ModInfo *initMod(char *modFolderName) {
+ModInfo *initMod(char *modID, ModApiHandle api) {
 	char modInfoBuffer[MAX_PATH] = {'\0'};
 	char modInfoPath[MAX_PATH] = {'\0'};
 
-	sprintf(modInfoPath, ".\\mods\\%s\\mod-info.txt", modFolderName);
+	sprintf(modInfoPath, ".\\mods\\%s\\mod-info.txt", modID);
 
 	// opens and reads info file, will be changed to fopen if I can get
 	// it working on windows
@@ -94,11 +85,11 @@ ModInfo *initMod(char *modFolderName) {
 
 	// reads mod info from mod-info.txt file
 	ModInfo *mod = malloc(sizeof(ModInfo));
-	ParseConfigFile(modInfoBuffer, modFolderName, mod);
+	ParseConfigFile(modInfoBuffer, modID, mod);
 
 	// loads entry dll
 	char EntryDLL[MAX_PATH];
-	sprintf(EntryDLL, ".\\mods\\%s\\%s", modFolderName, mod->entrydll);
+	sprintf(EntryDLL, ".\\mods\\%s\\%s", modID, mod->entrydll);
 	printf("entry path: %s\n", EntryDLL);
 	HINSTANCE hinstLib = LoadLibraryA(EntryDLL);
 	if (hinstLib != NULL) {
@@ -106,7 +97,7 @@ ModInfo *initMod(char *modFolderName) {
 
 		// calls init function if it exists
 		if (mod->init != NULL) {
-			mod->init();
+			mod->init(api);
 		} else {
 			printf("Mod '%s' is missing init function\n", mod->name);
 			panic()
@@ -118,16 +109,37 @@ ModInfo *initMod(char *modFolderName) {
 	} else {
 		printf("Error while loading mod '%s', no dll found - should be at "
 			 "mods\\%s\\%s\n",
-			 mod->name, modFolderName, mod->entrydll);
+			 mod->name, modID, mod->entrydll);
 		panic();
 	}
 }
 
-ModList *modlist;
+ModList *mod_list;
+ModApi api;
+
+ModList *getEnabledMods() { return mod_list; }
+
+typedef struct SharedResource {
+	char *key;
+	void *value;
+	struct SharedResource
+	    *next; // we want a linked list here as we aren't allowed to ever move
+		     // the data, and it's also a one time lookup so not so bad
+} SharedResource;
+
+SharedResource sharedResources = {};
+
+void *acquireSharedResource(char *key,
+				    void (*constructor)(void **resource_pointer)) {
+	SharedResource *searching = &sharedResources;
+}
+
 void initModLoader() {
-	modlist = malloc(sizeof(ModList));
-	modlist->length = 0;
-	modlist->mods = malloc(sizeof(ModInfo *));
+	api.getGameState = NULL;
+	api.getEnabledMods = getEnabledMods;
+	api.acquireSharedResource = acquireSharedResource;
+	mod_list->length = 0;
+	mod_list->mods = malloc(sizeof(*mod_list->mods));
 
 	WIN32_FIND_DATA fileData;
 	HANDLE findHandle = INVALID_HANDLE_VALUE;
@@ -141,8 +153,8 @@ void initModLoader() {
 			if ((fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) !=
 				  0 &&
 			    (fileData.cFileName[0] != '.')) {
-				ModInfo *mod = initMod(fileData.cFileName);
-				appendModToModList(modlist, mod);
+				ModInfo *mod = initMod(fileData.cFileName, &api);
+				appendModToModList(mod_list, mod);
 			}
 		}
 	}
@@ -151,6 +163,7 @@ void initModLoader() {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD nReason, LPVOID lpReserved) {
 
 	if (nReason == DLL_PROCESS_ATTACH) {
+		baseAddress = (size_t)GetModuleHandleA(NULL);
 		AllocConsole();
 		freopen("CONOUT$", "w", stdout);
 
